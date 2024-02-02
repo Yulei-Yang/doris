f20eea608e3f810658012f7f3d3eb0f8b99188ab
Yulei-Yang <yulei.yang0699@gmail.com>
2024-02-02 10:31:54 +0800

    [Tencent] fix catalog value in audit log table

--- fe/fe-core/src/main/java/org/apache/doris/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ConnectProcessor.java
@@ -303,6 +303,11 @@ public class ConnectProcessor {
                 .setWorkloadGroup(ctx.getWorkloadGroupName())
                 .setFuzzyVariables(!printFuzzyVariables ? "" : ctx.getSessionVariable().printFuzzyVariables());
 
+        // when doris fe is booting, current catalog may not be inited
+        if (ctx.getCurrentCatalog() != null) {
+            ctx.getAuditEventBuilder().setCatalog(ctx.getCurrentCatalog().getName());
+        }
+
         if (ctx.getState().isQuery()) {
             MetricRepo.COUNTER_QUERY_ALL.increase(1L);
             MetricRepo.USER_COUNTER_QUERY_ALL.getOrAdd(ctx.getQualifiedUser()).increase(1L);
